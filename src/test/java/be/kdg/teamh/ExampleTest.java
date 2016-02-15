5847230cd8219e098e41c42fcdd14a92388decb7
Kenneth De Win <knnethdw@gmail.com>
2016-02-15 14:17:50 +0100

    Add basic test

---
+++ src/test/java/be/kdg/teamh/ExampleTest.java
@@ -0,0 +1,14 @@
+package be.kdg.teamh;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExampleTest
+{
+    @Test
+    public void blablabla()
+    {
+        assertTrue(true);
+    }
+}
