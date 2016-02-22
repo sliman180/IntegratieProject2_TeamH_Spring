85cdd7e8c01149a7f6985a2a5d6408d1016c58d4
Mouzza <moussa.elb@hotmail.be>
2016-02-22 14:43:59 +0100

    Kaart test

---
+++ src/test/java/be/kdg/teamh/KaartTest.java
@@ -0,0 +1,14 @@
+package be.kdg.teamh;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class KaartTest {
+}
