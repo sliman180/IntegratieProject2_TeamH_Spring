d1923500c27ad5234940456e00f7d83eb32730d8
Kenneth De Win <knnethdw@gmail.com>
2016-03-07 20:09:23 +0100

    Change insert order

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-insert into gebruikers (gebruikersnaam, wachtwoord)
-values ('user', '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb'), ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
-
 insert into rollen (naam, beschrijving)
 values ('user', ''), ('admin', '');
+
+insert into gebruikers (gebruikersnaam, wachtwoord)
+values ('user', '04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb'), ('admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
 
 insert into gebruikers_rollen (gebruikers_id, rollen_id)
 values (1, 1), (2, 1), (2, 2);
